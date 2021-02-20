e4c55340ab12dae3990e13db66ad4b52cfbd1c01
Denwiliusz27 <danwielgosz@gmail.com>
2021-02-20 22:48:26 +0100

    add

--- PROJEKT_2/routes/index.js
+++ PROJEKT_2/routes/index.js
@@ -6,8 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/sala', function(req, res, next) {
-  res.render('index', { title: 'sala' });
-});
-
 module.exports = router;
